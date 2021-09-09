c201b47196b4bc2152f8105a943c3d582680f618
Daniel del Ser Martin <danieldelsermartin@gmail.com>
2021-09-10 01:00:39 +0200

    Direcitva personalizada myNgIf

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,26 +7,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class AgregarComponent implements OnInit {
+export class AgregarComponent {
 
-  colorQueQuiero: string = '#660099';
-  texto1: string = 'Daniel'
+  texto1: string = 'Daniel';
+  color : string = 'green';
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', Validators.required]
-  })
+    nombre: ['', Validators.required ]
+  });
 
   constructor( private fb: FormBuilder ) { }
-
-  ngOnInit(): void {
-  }
 
   tieneError( campo: string ): boolean {
     return this.miFormulario.get(campo)?.invalid || false;
   }
 
+
   cambiarNombre() {
-    this.texto1 = 'Don Daniel';
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
 
 }
